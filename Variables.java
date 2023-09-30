7459deb79f0dd2a54547056cf678a7e6efea3be5
Aliaksei Barouski <feliks1995@mail.ru>
2023-09-30 11:20:23 +0300

    add first 6 lessons

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 557;
+        short myShort = 3266;
+        long myLong = 2454545;
+        double myDounle = 235.35;
+        float myFloat = 4545.54f; // имеем ввиду именно Float поэтому вконце ставим букву f;
+
+        var c = "a" ;
+        boolean b = true; // boolean true ili false
+        byte myByte = 100; // -128 do 128
+
+        System.out.println(myInt);
+
+    }
+
+}
